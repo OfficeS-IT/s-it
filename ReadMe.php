cdcb796e9742cf357af40ae305fd2acbd3959bd8
SuzanaS-it <suzana.miskeljin@s-it.cc>
2018-11-26 12:13:02 +0100

    php readme

---
+++ ReadMe.php
@@ -0,0 +1,26 @@
+<?php
+$link = "about";
+include 'header.php' ?>
+
+    <div class="slider-bg">
+        <!-- Main component for a primary marketing message or call to action -->
+        <div class="container container-overlay2">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-12 about-items">
+                <h1>We are S-IT ENGINEERING</h1>
+                <p>We are creating innovative IT application which solve the needs and problems of a modern society.</p>
+                <p>Our products are saving both time and money for our customers through technologies of the 21st century.</p>
+                <p>We create web sites which successfuly realize business ideas.</p>
+                <p>We ensure the quality of IT systems with automatic and manual testing, because the precondition for a satisfied customer and business credibility comes only through high quality testing.
+                </p>
+            </div>
+        </div>
+
+        <div class="beautify"></div>
+
+    </div>
+    </div>
+
+
+<?php include 'footer.php' ?>
